5f7b75fa3e3b62e83098d69d60642fac7503b413
gfamanso <gabrielfamanso@hotmail.com>
2026-08-31 10:55:22 -0300

    exercício preço combustível atualizado.

--- precocombustivel.js
+++ precocombustivel.js
@@ -1,1 +1,8 @@
+const precoCombustivel = 6.89;
+const mediaConsumoCarro = 13;
+const distanciaPercorrida = 1580;
+const litrosConsumidos = distanciaPercorrida / mediaConsumoCarro;
+const totalGasto = litrosConsumidos * precoCombustivel;
 
+console.log(`Para percorrer 100 km foram gastos ${litrosConsumidos.toFixed(2)} litros de combustíveis a R$ ${precoCombustivel.toFixed(2)} o litro, totalizando R$ ${totalGasto.toFixed(2)}`);
+// console.log(totalGasto);
