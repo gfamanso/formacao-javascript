1edbcfa7d0014a2eeddd4dcf0c2e250c445357fa
gfamanso <gabrielfamanso@hotmail.com>
2026-09-14 22:15:44 -0300

    introdução ao conceito de DOM traversal (navegação na árvore do DOM). Abas interativas e barras de estatísticas dinâmicas usando classList, dataset, querySelectorAll e navegação no DOM

--- 07-modulo-html-javascript/assets/js/pokemon-details.js
+++ 07-modulo-html-javascript/assets/js/pokemon-details.js
@@ -2,6 +2,18 @@ const abas = document.querySelectorAll(".aba");
 
 const conteudos = document.querySelectorAll(".conteudo-aba");
 
+const preenchimentos = document.querySelectorAll(".preenchimento");
+
+const valores = [45, 49, 49, 65, 65, 45];
+
+console.log(preenchimentos[0].parentElement.parentElement.dataset.valor);
+
+valores.forEach(function (valor, index) {
+    preenchimentos[index].style.width = valor + "%";
+});
+
+
+
 abas.forEach(function (aba, index) {
     aba.addEventListener("click", function () {
 
@@ -18,4 +30,6 @@ abas.forEach(function (aba, index) {
         aba.classList.add("aba-ativa");
     });
 });
+
+console.log(preenchimentos);
 
